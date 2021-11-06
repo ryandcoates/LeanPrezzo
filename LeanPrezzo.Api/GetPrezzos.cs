468c462fbc999a9bcd89704ef118f13025c9d189
Ryan Coates <ryan.coates@gmail.com>
2021-11-05 20:18:30 -0600

    updates for jake

--- LeanPrezzo.Api/GetPrezzos.cs
+++ LeanPrezzo.Api/GetPrezzos.cs
@@ -14,7 +14,7 @@ namespace LeanPrezzo.Api
     {
         [FunctionName("GetPrezzos")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "prezzos")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
---
+++ LeanPrezzo.Data/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Data.Tables;
+
+namespace LeanPrezzo.Data
+{
+    public class Class1
+    {
+
+        // Construct a new <see cref="TableClient" /> using a <see cref="TableSharedKeyCredential" />.
+        TableClient tableClient = new TableClient(
+            new Uri("http://localhost:10002/devstorageaccount1/table1"),
+            "table1",
+            new TableSharedKeyCredential("Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="));
+
+        // Create the table in the service.
+        tableClient.Create();
+
+    }
+}
